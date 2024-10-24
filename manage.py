6899a82403e69556de986f030cca563fd3fc6207
webtjk <155659716+webtjk@users.noreply.github.com>
2024-10-24 19:03:05 +0600

    flask

---
+++ manage.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+a = int(input("How old are you?"))
+while a < 18:
+    print("OH baby bye")
+if a > 18:
+    print("Hi bro what do you want? ")
+    int(input(" "))
+
+        
+
+    
